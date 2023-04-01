663471c6ce3dd801c4c32ba17a4d88857c8d85d1
Wilson Chen <wilson@Wilsons-MBP.local>
2023-04-01 00:31:31 -0700

    Home page layout

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,21 +1,22 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavBar() {
     return (
-        <Navbar collapseOnSelect expand="lg" style={{background: "linear-gradient(90deg, rgba(15,65,167,1) 0%, rgba(15,188,191,1) 100%)"}}>
-            <Container>
-                <Navbar.Brand href="#home">PropertyApp</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#register">Register</Nav.Link>
-                        <Nav.Link href="#login">Login</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+        <Navbar collapseOnSelect expand="lg" style={{ background: "white", marginInline: "2%" }}>
+            <Navbar.Brand href="#home" style={{ fontFamily: "Lato", fontWeight: 'bold' }}>HouseConnect</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#resources">Resources</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/register">Register</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap" rel="stylesheet"></link>
     <App />
   </React.StrictMode>
 );
