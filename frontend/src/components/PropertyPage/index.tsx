f0aadc9aec993a4e1d3aa961b914728940b30e21
Wilson <wilsonchen.code@gmail.com>
2023-04-19 15:07:00 -0700

    Mobile responsive sidenav, property page skeleton done

--- frontend/src/components/PropertyPage/index.tsx
+++ frontend/src/components/PropertyPage/index.tsx
@@ -1,51 +1,101 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProperties } from "../../config/ApiService";
 import SideNav from "../SideNav";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Card } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useMediaQuery } from 'react-responsive';
 
 export default function PropertyPage() {
     const { status, data } = useQuery({
         queryKey: ['properties'],
         queryFn: getProperties,
-    })
+    });
+    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 768px)' });
     if (status === 'loading') {
-        return <span>Loading...</span>
+        return <span>Loading...</span>;
     }
     if (status === 'error') {
-        return <span>Unexpected error</span>
+        return <span>Unexpected error</span>;
     }
     if (data?.status !== 200) {
-        return <div>{data?.data}</div>
+        return <div>{data?.data}</div>;
     }
     return (
         <Container fluid style={{ height: '100vh' }}>
             <Row>
                 <SideNav link={'/properties'} />
                 <Col className="px-0">
-                    {data?.data.length === 0 ? <div>No properties</div> :
-                        <div className="table-responsive">
-                            <table className="table table-striped table-hover">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Address</th>
-                                        <th scope="col">City</th>
-                                        <th scope="col">State</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {data?.data.map((property: any) => {
-                                        return (
-                                            <tr>
-                                                <td>{property.address}</td>
-                                                <td>{property.city}</td>
-                                                <td>{property.state}</td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                    }
+                    <Container fluid>
+                        <Navbar expand="md" className="my-2">
+                            <Container fluid>
+                                {!isDesktopOrLaptop && <Navbar.Brand href="#">Properties</Navbar.Brand>}
+                                <Navbar.Toggle aria-controls="navbarScroll" />
+                                <Navbar.Collapse id="navbarScroll">
+                                    <Nav
+                                        className="me-auto my-2 my-lg-0"
+                                        style={{ maxHeight: '100px' }}
+                                        navbarScroll
+                                    >
+                                        <NavDropdown title="Sort By" id="navbarScrollingDropdown" style={{ fontWeight: 'bold' }}>
+                                            <NavDropdown.Item href="#action1">City</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action2">State</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action3">Size</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action4">Type</NavDropdown.Item>
+                                        </NavDropdown>
+                                    </Nav>
+                                    <Form className="d-flex">
+                                        <Form.Control
+                                            type="search"
+                                            placeholder="Search"
+                                            className="me-2"
+                                            aria-label="Search"
+                                        />
+                                        <Button variant="outline-success">Search</Button>
+                                    </Form>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+                        <hr style={{ border: '1px solid black' }} />
+                        <Card>
+                            <div className="table-responsive">
+                                <table className="table table-striped table-hover my-0">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Address</th>
+                                            <th scope="col">City</th>
+                                            <th scope="col">State</th>
+                                            <th scope="col">Type</th>
+                                            <th scope="col">Size</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data?.data.map((property: any) => {
+                                            return (
+                                                <tr>
+                                                    <td>{property.address}</td>
+                                                    <td>{property.city}</td>
+                                                    <td>{property.state}</td>
+                                                    <td>{property.type}</td>
+                                                    <td>{property.size}</td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <td colSpan={5}>
+                                                <Button> Create new property</Button>
+                                            </td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </Card>
+                    </Container>
                 </Col>
             </Row>
         </Container>
--- frontend/src/components/SideNav/index.tsx
+++ frontend/src/components/SideNav/index.tsx
@@ -2,65 +2,70 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
 import { logoutUser } from '../../config/ApiService';
-import { useMutation } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from "react-router-dom";
-import './SideNav.css'
+import { useMediaQuery } from 'react-responsive';
+import { Col } from "react-bootstrap";
+import './SideNav.css';
 
 interface NavBarProps {
-    link: string
+    link: string;
 }
 
 export default function SideNav(props: NavBarProps) {
     const [active, setActive] = useState<string>('/');
+    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 768px)' });
     const navigate = useNavigate();
     const { mutateAsync } = useMutation({
         mutationFn: logoutUser,
     });
     const FormSubmit = async () => {
         const response = await mutateAsync();
-        if(response?.status === 200){
+        if (response?.status === 200) {
             navigate(`/`);
         }
-    }
+    };
 
-    useEffect (() => {
+    useEffect(() => {
         setActive(props.link);
-    }, [props.link])
+    }, [props.link]);
 
     return (
-        <Nav activeKey={active} variant="pills" defaultActiveKey="/" className="flex-column" style={{ background: '#19376D', minHeight: '100vh', width: '250px', padding: 0 }}>
-            <Navbar.Brand href="/" style={{ fontFamily: "Lato", fontWeight: 'bold', color: 'white', marginBlock: 20 }}>
-                <i className="bi bi-buildings-fill" style={{ marginRight: 10 }}></i>
-                HouseConnect
-            </Navbar.Brand>
-            <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/dashboard">
-                <i className="bi bi-ui-checks-grid" style={{ marginRight: 20 }}></i>
-                Dashboard
-            </Nav.Link>
-            <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/properties">
-                <i className="bi bi-houses-fill" style={{ marginRight: 20 }}></i>
-                Properties
-            </Nav.Link>
-            <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/tenants">
-                <i className="bi bi-people-fill" style={{ marginRight: 20 }}></i>
-                Tenants
-            </Nav.Link>
-            <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/leases">
-                <i className="bi bi-file-text-fill" style={{ marginRight: 20 }}></i>
-                Leases
-            </Nav.Link>
-            <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/tickets">
-                <i className="bi bi-ticket-detailed-fill" style={{ marginRight: 20 }}></i>
-                Tickets
-            </Nav.Link>
-            <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/profile">
-                <i className="bi bi-person-circle" style={{ marginRight: 20 }}></i>
-                Profile
-            </Nav.Link>
-            <Nav.Link className="mt-auto" style={{ color: 'white', margin: 10 }} onClick={FormSubmit}>
-                <i className="bi bi-box-arrow-left" style={{ marginRight: 20 }}></i>
-                Sign Out
-            </Nav.Link>
-        </Nav>
+        <Col className="px-0" xs='auto' style={{width: isDesktopOrLaptop ? 300 : 'auto'}} >
+            <Nav activeKey={active} variant="pills" defaultActiveKey="/" className="flex-column" style={{ background: '#19376D', minHeight: '100vh', padding: 0 }}>
+                <Navbar.Brand href="/" style={{ fontFamily: "Lato", fontWeight: 'bold', color: 'white', marginBlock: 20 }}>
+                    <i className="bi bi-buildings-fill" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>HouseConnect</>}
+                </Navbar.Brand>
+                <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/dashboard">
+                    <i className="bi bi-ui-checks-grid" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Dashboard</>}
+                </Nav.Link>
+                <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/properties">
+                    <i className="bi bi-houses-fill" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Properties</>}
+                </Nav.Link>
+                <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/tenants">
+                    <i className="bi bi-people-fill" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Tenants</>}
+                </Nav.Link>
+                <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/leases">
+                    <i className="bi bi-file-text-fill" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Leases</>}
+                </Nav.Link>
+                <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/tickets">
+                    <i className="bi bi-ticket-detailed-fill" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Tickets</>}
+                </Nav.Link>
+                <Nav.Link style={{ color: 'white', margin: 10, display: 'flex' }} href="/profile">
+                    <i className="bi bi-person-circle" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Profile</>}
+                </Nav.Link>
+                <Nav.Link className="mt-auto" style={{ color: 'white', display: 'flex', margin: 10 }} onClick={FormSubmit}>
+                    <i className="bi bi-box-arrow-left" style={{ marginRight: isDesktopOrLaptop ? 20 : 0 }}></i>
+                    {isDesktopOrLaptop && <>Sign Out</>}
+                </Nav.Link>
+            </Nav>
+        </Col>
     );
 }
