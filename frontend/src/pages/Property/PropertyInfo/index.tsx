abac53471484da2fa37cc85dc32b17130cf1e4ec
Wilson Chen <wilson@Wilsons-MBP.local>
2023-05-11 20:10:01 -0700

    Property info page skeleton

--- frontend/src/pages/Property/PropertyInfo/index.tsx
+++ frontend/src/pages/Property/PropertyInfo/index.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProperty } from "../../../utils/ApiService";
 import SideNav from "../../../components/SideNav";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 
 export default function PropertyInfoPage() {
@@ -28,8 +29,9 @@ export default function PropertyInfoPage() {
                 <SideNav link={'/properties'} />
                 <Col className="px-0" style={{ background: '#ebecf0' }}>
                     <Container fluid>
-                        <Col>
-                            <h2>Property </h2>
+                        <Col style={{marginTop: '24px', display: 'flex', justifyContent: 'center'}}>
+                            <h2>Manage Property</h2>
+                            <Button style={{marginInline: 20}}><i className="bi bi-pen"></i></Button>
                         </Col>
                         <hr />
                         <Col>
@@ -41,10 +43,43 @@ export default function PropertyInfoPage() {
                         <Col>
                             State: {data?.data.state}
                         </Col>
-                        <Col>
-                            Tenants: {data?.data.tenants ?  data?.data.tenants : 'No Tenants'}
-                        </Col>
                         <hr />
+                        <Container fluid className="overflow-auto" style={{ height: '80vh' }}>
+                            <Row lg={2} xs={1} className="gy-5">
+                                <Col style={{ height: '375px' }}>
+                                    <Card className="h-100">
+                                        <h6 style={{ marginTop: 16, marginBottom: 0 }}>Monthly Revenue</h6>
+                                        <hr style={{ border: '1px solid black' }} />
+                                        <div>
+                                            Graph
+                                        </div>
+                                    </Card>
+                                </Col>
+                                <Col style={{ height: '375px' }}>
+                                    <Card className="h-100">
+                                        <h6 style={{ marginTop: 16, marginBottom: 0 }}>Tenants</h6>
+                                        <hr style={{ border: '1px solid black' }} />
+                                        <div>
+                                            Table
+                                        </div>
+                                    </Card>
+                                </Col>
+                                <Col style={{ height: '375px' }}>
+                                    <Card className="h-100">
+                                        <h6 style={{ marginTop: 16, marginBottom: 0 }}>Leases</h6>
+                                        <hr style={{ border: '1px solid black' }} />
+                                        <div>Table</div>
+                                    </Card>
+                                </Col>
+                                <Col style={{ height: '375px' }}>
+                                    <Card className="h-100">
+                                        <h6 style={{ marginTop: 16, marginBottom: 0 }}>Tickets</h6>
+                                        <hr style={{ border: '1px solid black' }} />
+                                        <div>Table</div>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Container>
                 </Col>
             </Row>
